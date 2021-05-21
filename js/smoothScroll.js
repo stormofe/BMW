a5dadc5f7e50d81e9d416d1d9895665adb1dd064
Sergey Pr <sergeyvp5130@gmail.com>
2021-05-21 12:15:55 +0300

    add sendForm

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,3 +1,4 @@
+/*
 const smoothScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
 
 smoothScrollElems.forEach(link => {
@@ -10,3 +11,41 @@ smoothScrollElems.forEach(link => {
 		});
 	})
 })
+*/
+const SPEED = 0.2;
+const scrolled = event => {
+	event.preventDefault();
+
+	const target = event.target;
+
+	if (target.matches('[href^="#"]')) {
+		let start = 0;
+		const pageY = window.pageYOffset;
+
+		const hash = target.getAttribute('href');
+
+		if (hash === '#') return
+
+		const elem = document.querySelector(hash); //take name of section
+
+		const coordinateElem = elem.getBoundingClientRect().top; //take coordinats of section (top - 20px) if need indent
+
+		const step = time => {
+			if (!start) start = time;
+
+			const progress = time - start;
+
+			const r = (coordinateElem < 0 ?
+				Math.max(pageY - progress / SPEED, pageY + coordinateElem) :
+				Math.min(pageY + progress / SPEED, pageY + coordinateElem)
+			)
+
+			window.scrollTo(0, r);
+
+			if (r < pageY + coordinateElem) requestAnimationFrame(step);
+		}
+		requestAnimationFrame(step)
+	}
+}
+
+document.body.addEventListener('click', scrolled);
